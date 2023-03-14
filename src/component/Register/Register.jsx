37fde821ee4c6d366cc4c9fb9ef3033dab2e989e
TK17250 <TXK285@gmail.com>
2023-03-14 20:37:13 +0700

    add Error password

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -5,6 +5,7 @@ import '../Login/login.css';
 
 function Register() {
 
+    //  show Password
     function showpassword() {
         let password = document.getElementById("password")
         let password1 = document.getElementById("password1")
@@ -18,6 +19,24 @@ function Register() {
         }
     }
 
+    //  Error
+    function Errorpassword(event) {
+        let passwordnumber = document.getElementById("password")
+        let Errorpassword = document.getElementsById("error")
+
+        let Long = false
+        passwordnumber.forEach(password => {
+            if (password.value.length > 8) {
+                Long = true
+            }
+        });
+
+        if (Long) {
+            Errorpassword.style.display = "block"
+            event.preventDefault();
+        }
+    }
+
     return (
         <Form className='container'>
             <h4>Register</h4>
@@ -26,29 +45,36 @@ function Register() {
                     <Form.Label>Username</Form.Label>
                     <Form.Control type="text" placeholder="ใส่ชื่อของคุณ" />
                 </Form.Group>
+
                 {/*             Email            */}
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>You Email</Form.Label>
                     <Form.Control type="Email" placeholder="อีเมลของคุณ" />
                 </Form.Group>
+
                 {/*          Password 1           */}
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" id='password' placeholder="ใส่รหัสผ่าน" />
+                    <Form.Control type="password" id='password' onInput={Errorpassword} placeholder="ใส่รหัสผ่าน" />
+                    <p id='error' style={{display: "none"}}>Error</p>
                 </Form.Group>
+
                 {/*          Password 2           */}
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Confirm password</Form.Label>
                     <Form.Control type="password" id='password1' placeholder="ยืนยันรหัสผ่าน" />
                 </Form.Group>
+
                 {/*             Show Password            */}
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" onClick={showpassword} label="แสดงรหัสผ่าน" />
                 </Form.Group>
+
                 {/*             Submit           */}
                 <Button variant="info" type="submit" className='m-auto But d-block'>
                     สมัครสมาชิก
                 </Button>
+
                 {/*       Link register      */}
                 <Link to="/Login" className='Register'>มีบัญชีแล้ว</Link>
             </div>
